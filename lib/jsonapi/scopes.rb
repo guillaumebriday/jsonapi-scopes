3bcf62492fd4d5c5accd167a00403c984efd5cad
Guillaume Briday <guillaumebriday@gmail.com>
2019-05-11 00:18:02 +0200

    Adding filtering module

--- lib/jsonapi/scopes.rb
+++ lib/jsonapi/scopes.rb
@@ -2,6 +2,30 @@ require "jsonapi/scopes/railtie"
 
 module Jsonapi
   module Scopes
-    # Your code goes here...
+    extend ActiveSupport::Concern
+
+    included do
+      @filters ||= []
+    end
+
+    module ClassMethods
+      attr_reader :filters
+
+      def filter(name, *args)
+        scope(name, *args)
+        @filters << name
+      end
+
+      def apply_filter(params)
+        results = self
+        filtering_params = params.dig(:filter) || {}
+
+        filtering_params.each do |key, value|
+          results = results.public_send(key, value) if value.present? && @filters.include?(key.to_sym)
+        end
+
+        results
+      end
+    end
   end
 end
